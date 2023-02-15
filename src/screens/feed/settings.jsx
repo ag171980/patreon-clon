9481e293c711209c97a183dfe8dc75aa5ef57d6d
Alexis Gutiérrez <agutierrez@tecnoaccion.corp>
2023-02-15 16:49:22 -0300

    trabajo en el responsive de settings

--- src/screens/feed/settings.jsx
+++ src/screens/feed/settings.jsx
@@ -21,32 +21,24 @@ const Settings = () => {
 
         <div className="flex">
             <Sidebar />
-            <div className="w-full h-screen flex flex-col justify-start items-center xxs:pt-7 pt-10 bg-slate-100">
+            <div className="w-full min-h-screen flex flex-col justify-start items-center xxs:pt-7 pt-10 bg-slate-100">
                 <h1 className=" font-bold text-3xl xxs:text-base xxs:self-center xl:text-3xl xl:self-center">Settings</h1>
-                <div className="tabs flex flex-row justify-center xxs:overflow-x-scroll xxs:w-screen xxs:justify-start items-center my-4">
-                    <p className={`p-2 px-0 mx-6 xxs:text-ellipsis xxs:whitespace-pre xxs:mx-4 text-sm hover:cursor-pointer ${(tab === "Basics") ? "border-b-2 border-violet-500" : ""}`} onClick={() => {
+                <div className="tabs flex flex-row xxs:overflow-x-scroll xxs:w-screen xxs:justify-center items-center my-4">
+                    <p className={`p-2 px-0 mx-6 xxs:text-ellipsis xxs:whitespace-pre xxs:mx-4 text-sm hover:cursor-pointer ${(tab === "Basics") ? "text-violet-600 font-semibold border-b-2 border-violet-500" : ""}`} onClick={() => {
                         setActualTab(<Basics />)
                         setTab("Basics")
                     }}>Basics</p>
-                    <p className={`p-2 px-0 mx-6 xxs:text-ellipsis xxs:whitespace-pre xxs:mx-4 text-sm hover:cursor-pointer ${(tab === "Account") ? "border-b-2 border-violet-500" : ""}`} onClick={() => {
+                    <p className={`p-2 px-0 mx-6 xxs:text-ellipsis xxs:whitespace-pre xxs:mx-4 text-sm hover:cursor-pointer ${(tab === "Account") ? " text-violet-600 font-semibold border-b-2 border-violet-500" : ""}`} onClick={() => {
                         setActualTab(<Account />)
                         setTab("Account")
                     }}>Account</p>
-                    <p className={`p-2 px-0 mx-6 xxs:text-ellipsis xxs:whitespace-pre xxs:mx-4 text-sm hover:cursor-pointer ${(tab === "Email Notifications") ? "border-b-2 border-violet-500" : ""}`} onClick={() => {
-                        setActualTab(<EmailNotifications />)
-                        setTab("Email Notifications")
-                    }}>Email notifications</p>
-                    <p className={`p-2 px-0 mx-6 xxs:text-ellipsis xxs:whitespace-pre xxs:mx-4 text-sm hover:cursor-pointer ${(tab === "Email Notifications") ? "border-b-2 border-violet-500" : ""}`} onClick={() => {
-                        setActualTab(<EmailNotifications />)
-                        setTab("Email Notifications")
-                    }}>Email notifications</p>
-                    <p className={`p-2 px-0 mx-6 xxs:text-ellipsis xxs:whitespace-pre xxs:mx-4 text-sm hover:cursor-pointer ${(tab === "Email Notifications") ? "border-b-2 border-violet-500" : ""}`} onClick={() => {
+                    <p className={`p-2 px-0 mx-6 xxs:text-ellipsis xxs:whitespace-pre xxs:mx-4 text-sm hover:cursor-pointer ${(tab === "Email Notifications") ? "text-violet-600 font-semibold border-b-2 border-violet-500" : ""}`} onClick={() => {
                         setActualTab(<EmailNotifications />)
                         setTab("Email Notifications")
                     }}>Email notifications</p>
                 </div>
-                <div className="content-tab">
-                    {/* {actualTab} */}
+                <div className="content-tab xxs:w-screen flex items-center justify-center">
+                    {actualTab}
                 </div>
             </div>
         </div>
